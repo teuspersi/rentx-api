bc21375ed5e144ec9adb9ab5a2dd057f1937ffbe
Mateus <teuspersi@gmail.com>
2021-07-27 17:27:14 -0300

    Add tests for the AuthenticateUser use case

---
+++ src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -0,0 +1,37 @@
+/* eslint-disable no-shadow */
+/* eslint-disable camelcase */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable class-methods-use-this */
+import ICreateUserDTO from '../../dtos/ICreateUserDTO';
+import User from '../../entities/User';
+import IUsersRepository from '../IUsersRepository';
+
+export default class UsersRepositoryInMemory implements IUsersRepository {
+  users: User[] = [];
+
+  async create({
+    driver_license,
+    email,
+    name,
+    password,
+  }: ICreateUserDTO): Promise<void> {
+    const user = new User();
+
+    Object.assign(user, {
+      driver_license,
+      email,
+      name,
+      password,
+    });
+
+    this.users.push(user);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return this.users.find(user => user.email === email);
+  }
+
+  async findById(id: string): Promise<User> {
+    return this.users.find(user => user.id === id);
+  }
+}
