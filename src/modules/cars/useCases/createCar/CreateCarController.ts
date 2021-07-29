6f1e3ed83b2b2a4a2390affaa5bbea9f5f14f564
Mateus <teuspersi@gmail.com>
2021-07-28 21:25:18 -0300

    Add Car controller

---
+++ src/modules/cars/useCases/createCar/CreateCarController.ts
@@ -0,0 +1,33 @@
+/* eslint-disable camelcase */
+/* eslint-disable class-methods-use-this */
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import CreateCarUseCase from './CreateCarUseCase';
+
+export default class CreateCarController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const {
+      name,
+      description,
+      daily_rate,
+      license_plate,
+      fine_amount,
+      brand,
+      category_id,
+    } = request.body;
+
+    const createCarUseCase = container.resolve(CreateCarUseCase);
+
+    const car = await createCarUseCase.execute({
+      name,
+      description,
+      daily_rate,
+      license_plate,
+      fine_amount,
+      brand,
+      category_id,
+    });
+
+    return response.status(201).json(car);
+  }
+}
