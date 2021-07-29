29cf1f3e24b34b5c4238cba9177de0f33fcff07e
Mateus <teuspersi@gmail.com>
2021-07-29 16:13:09 -0300

    Create Specification_cars migration

---
+++ src/shared/infra/typeorm/migrations/1627584639401-CreateSpecificationsCars.ts
@@ -0,0 +1,73 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable import/prefer-default-export */
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export class CreateSpecificationsCars1627584639401
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'specifications_cars',
+        columns: [
+          {
+            name: 'car_id',
+            type: 'uuid',
+          },
+          {
+            name: 'specification_id',
+            type: 'uuid',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'specifications_cars',
+      new TableForeignKey({
+        name: 'FKSpecifiationCar',
+        referencedTableName: 'specifications',
+        referencedColumnNames: ['id'],
+        columnNames: ['specification_id'],
+        onDelete: 'SET NULL',
+        onUpdate: 'SET NULL',
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'specifications_cars',
+      new TableForeignKey({
+        name: 'FKCarSpecification',
+        referencedTableName: 'cars',
+        referencedColumnNames: ['id'],
+        columnNames: ['car_id'],
+        onDelete: 'SET NULL',
+        onUpdate: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey(
+      'specifications_cars',
+      'FKCarSpecification',
+    );
+
+    await queryRunner.dropForeignKey(
+      'specifications_cars',
+      'FKSpecifiationCar',
+    );
+
+    await queryRunner.dropTable('specifications_cars');
+  }
+}
