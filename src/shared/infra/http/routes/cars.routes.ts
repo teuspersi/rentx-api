f7b42f0c4b3b3a965471119e5adf8bf37a1902c5
Mateus <teuspersi@gmail.com>
2021-07-29 16:12:49 -0300

    Add ListAvailableCars route

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 
 import CreateCarController from '@modules/cars/useCases/createCar/CreateCarController';
+import ListAvailableCarsController from '@modules/cars/useCases/listAvailableCars/ListAvailableCarsController';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import { ensureAdmin } from '../middlewares/ensureAdmin';
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
+const listAvailableCarsController = new ListAvailableCarsController();
 
 carsRoutes.post(
   '/',
@@ -14,5 +16,7 @@ carsRoutes.post(
   ensureAdmin,
   createCarController.handle,
 );
+
+carsRoutes.get('/available', listAvailableCarsController.handle);
 
 export default carsRoutes;
