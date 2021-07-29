a24b7b4f6fda5e1a95cdc04ed39d630ec0f34a0c
Mateus <teuspersi@gmail.com>
2021-07-28 21:28:31 -0300

    Add CarsRepository to the index

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -8,6 +8,8 @@ import IUsersRepository from '@modules/accounts/repositories/IUsersRepository';
 
 import { SpecificationsRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository';
 import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import CarsRepository from '@modules/cars/infra/typeorm/repositories/CarsRepository';
 
 container.registerSingleton<ICategoriesRepository>(
   'CategoriesRepository',
@@ -23,3 +25,5 @@ container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
 );
+
+container.registerSingleton<ICarsRepository>('CarsRepository', CarsRepository);
