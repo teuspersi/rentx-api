e2f938d0d5053519e5357c052e53d1dec9d84358
Mateus <teuspersi@gmail.com>
2021-07-28 22:49:14 -0300

    Add seed:admin

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,12 +1,15 @@
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
-import '../typeorm';
-import '@shared/container';
+
 import AppError from '@shared/errors/AppError';
+import createConnection from '../typeorm';
+import '@shared/container';
+
 import { router } from './routes';
 import swaggerFile from '../../../swagger.json';
 
+createConnection();
 const app = express();
 
 app.use(express.json());
--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,4 +1,4 @@
-import { createConnection, getConnectionOptions } from 'typeorm';
+import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
 interface IOptions {
   host: string;
@@ -6,8 +6,14 @@ interface IOptions {
 
 getConnectionOptions().then(options => {
   const newOptions = options as IOptions;
-  newOptions.host = 'database_ignite'; // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+  newOptions.host = 'localhost'; // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
   createConnection({
     ...options,
   });
 });
+
+export default async (host = 'database_ignite'): Promise<Connection> => {
+  const defaultOptions = await getConnectionOptions();
+
+  return createConnection(Object.assign(defaultOptions, { host }));
+};
