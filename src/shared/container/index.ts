36b84fed7a8c227a3eb020cbcc68e9da13d7d740
Mateus <teuspersi@gmail.com>
2021-07-30 23:50:57 -0300

    Create Rental controller, routes and add db support to create rental

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,7 @@
 import { container } from 'tsyringe';
 
+import '@shared/container/providers';
+
 import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
 import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
 
@@ -12,6 +14,8 @@ import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
 import CarsRepository from '@modules/cars/infra/typeorm/repositories/CarsRepository';
 import ICarsImagesRepository from '@modules/cars/repositories/ICarsImagesRepository';
 import CarsImageRepository from '@modules/cars/infra/typeorm/repositories/CarsImagesRepository';
+import IRentalsRepository from '@modules/rentals/repositories/IRentalsRepository';
+import RentalsRepository from '@modules/rentals/infra/typeorm/repositories/RentalsRepository';
 
 container.registerSingleton<ICategoriesRepository>(
   'CategoriesRepository',
@@ -33,4 +37,9 @@ container.registerSingleton<ICarsRepository>('CarsRepository', CarsRepository);
 container.registerSingleton<ICarsImagesRepository>(
   'CarsImageRepository',
   CarsImageRepository,
+);
+
+container.registerSingleton<IRentalsRepository>(
+  'RentalsRepository',
+  RentalsRepository,
 );
